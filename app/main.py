300ec27042192ab173aaffe04fae7b3b965d48a6
Akshay Channabasaiah <aksc.champ@gmail.com>
2024-10-29 11:36:26 -0400

    Updated CORS to add deployed website

--- app/main.py
+++ app/main.py
@@ -27,7 +27,9 @@ origins = [
     "https://localhost:5173",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://127.0.0.1:5173"
+    "https://127.0.0.1:5173",
+    "https://vividroi.com",
+    "https://www.vividroi.com"
 ]
 
 app.add_middleware(
